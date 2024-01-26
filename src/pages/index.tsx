a912097493b8b681ff6073529bb30af7632b48c5
CyberSleeper <mahartha.gemilang@gmail.com>
2024-01-26 18:04:50 +0700

    Add light on mouse position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import * as THREE from 'three'
 import * as React from 'react'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame  } from '@react-three/fiber'
+import { useRef, useState, useLayoutEffect } from 'react'
+import { Canvas, useFrame, useThree  } from '@react-three/fiber'
 import OrbitControls from '@/components/orbitControls'
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
 
   return (
     <mesh
@@ -17,25 +17,137 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onPointerOut={(event) => hover(false)}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 
-export default function App() {
+function Sphere(props: JSX.IntrinsicElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+    >
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  )
+}
+
+function Light(props: JSX.IntrinsicElements['mesh']) {
+  const [mouse, setMouse] = useState({ x: 0, y: 0 })
+  const canvas = useThree(state => state.gl.domElement)
+
+  useLayoutEffect(() => {
+    const updateMousePosition = (event: MouseEvent) => {
+      const rect = canvas.getBoundingClientRect()
+      const x = ((event.clientX - rect.left) / rect.width) * 2 - 1
+      const y = -((event.clientY - rect.top) / rect.height) * 2 + 1
+      console.log(x, y)
+      console.log(rect.width, rect.height)
+      setMouse({ x, y })
+    }
+
+    canvas.addEventListener('mousemove', updateMousePosition)
+
+    return () => {
+      canvas.removeEventListener('mousemove', updateMousePosition)
+    }
+  }, [canvas])
 
   return (
-    <div className='h-screen'>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-        <OrbitControls />
-      </Canvas>
+    <mesh>
+      <pointLight position={[mouse.x * 5, mouse.y * 5, 2]} intensity={5} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <div className='min-h-screen w-full'>
+      <div className='h-screen w-full top-0 left-0 fixed'>
+        <Canvas className='w-full h-screen -z-10'>
+          {/* <axesHelper args={[5]} /> */}
+          {/* <ambientLight intensity={0.5} /> */}
+          <Light />
+          {/* <Box position={[-3, 0, 0]} /> */}
+          {/* <Box position={[3, 0, 0]} /> */}
+          <Sphere position={[0, -2, 0]} />
+          {/* <OrbitControls /> */}
+        </Canvas>
+      </div>
+      <div className='h-screen'>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+        LOREM IPSUM<br/>
+      </div>
     </div>
   )
 }
