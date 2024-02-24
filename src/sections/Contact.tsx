c27b9020a2d9367807969ab2b29039ed3029d968
CyberSleeper <mahartha.gemilang@gmail.com>
2024-02-24 18:59:25 +0700

    Add Contact Title

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,7 +1,11 @@
+import FadeIn from "@/components/animations/fadeIn";
 
 export default function Contact() {
   return (
     <div className="transition-all min-h-screen font-satoshi pb-24 bg-light-background dark:bg-dark-background px-24 pt-16 flex flex-col">
+      <FadeIn>
+        <h1 className="text-5xl font-bold text-light-secondary dark:text-dark-secondary">Contact Me!</h1>
+      </FadeIn>
     </div>
   )
 }
