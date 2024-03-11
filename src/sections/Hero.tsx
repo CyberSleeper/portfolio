8cecaab7ae77a8d454464907618e6665eb12eed1
CyberSleeper <mahartha.gemilang@gmail.com>
2024-03-11 21:13:59 +0700

    Add responsiveness

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -16,10 +16,10 @@ export default function Hero() {
 
   return (
     <section className="font-satoshi min-h-screen">
-      <div className='flex flex-col justify-center h-screen px-24'>
+      <div className='flex flex-col justify-center h-screen md:px-24 px-8'>
         <div className='flex flex-col'>
-          <h2 className='text-6xl font-bold outline-dark-text outline-4'>Mahartha Gemilang</h2>
-          <h1 style={{ WebkitTextFillColor: 'transparent' }} className='bg-gradient-to-br from-dark-accent to-dark-primary mb-8 h-52 text-8xl font-bold whitespace-pre-line bg-clip-text'>
+          <h2 className='tracking-tight md:text-6xl text-xl font-bold outline-dark-text outline-4'>Mahartha Gemilang</h2>
+          <h1 style={{ WebkitTextFillColor: 'transparent' }} className='tracking-tight bg-gradient-to-br from-dark-accent to-dark-primary md:mb-8 md:h-52 h-32 md:text-8xl text-5xl font-bold whitespace-pre-line bg-clip-text'>
             <TypeAnimation
               sequence={roles}
               wrapper="span"
@@ -27,9 +27,9 @@ export default function Hero() {
               repeat={Infinity}
             />
           </h1>
-          <div className="flex gap-4">
+          <div className="flex md:flex-row flex-col md:w-auto gap-4">
             <div
-              className="text-xl px-6 py-4 w-fit rounded-xl text-black bg-dark-primary hover:bg-dark-accent font-bold hover:cursor-pointer hover:scale-105 active:scale-90 transition-all"
+              className="text-center md:text-xl text-lg px-6 md:py-4 py-3 md:w-fit w-full rounded-xl text-black bg-dark-primary hover:bg-dark-accent font-bold hover:cursor-pointer hover:scale-105 active:scale-90 transition-all"
               onClick={() => {
                 gsap.to(window, { duration: 2, scrollTo: { y: "#about", offsetY: 150 }, ease: "circ.out"});
               }}
@@ -42,7 +42,7 @@ export default function Hero() {
               rel="noreferrer"
             >
               <div
-                className="text-xl px-6 py-4 w-fit rounded-xl text-almond opacity-95 border-almond border-2 bg-transparent hover:text-dark-accent  font-bold hover:cursor-pointer hover:scale-105 active:scale-90 transition-all"
+                className="text-center md:text-xl text-lg px-6 md:py-4 py-3 md:w-fit w-full rounded-xl text-almond opacity-95 border-almond border-2 bg-transparent hover:text-dark-accent font-bold hover:cursor-pointer hover:scale-105 active:scale-90 transition-all"
               >
                 View Resume
               </div>
