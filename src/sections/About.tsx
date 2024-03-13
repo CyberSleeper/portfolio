5298dd202a535ca7805500a51a434be037912c17
CyberSleeper <mahartha.gemilang@gmail.com>
2024-03-13 22:58:39 +0700

    Add responsive to About

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -43,7 +43,7 @@ export default function About() {
   })
 
   return (
-    <div ref={curved} className="-mt-48 transition-all font-satoshi pb-24 bg-light-background dark:bg-dark-background px-24 pt-16 flex flex-col items-center"
+    <div ref={curved} className="-mt-48 transition-all font-satoshi pb-24 bg-light-background dark:bg-dark-background md:px-24 px-12 pt-16 flex flex-col items-center"
       style={{
         left: "-3%",
         borderTopLeftRadius: "100% 50%",
@@ -51,12 +51,12 @@ export default function About() {
       }}
     >
       <FadeIn>
-        <h1 className="text-5xl font-bold text-light-text dark:text-dark-text">About Me</h1>
+        <h1 className="md:text-5xl text-3xl font-bold text-light-text dark:text-dark-text">About Me</h1>
       </FadeIn>
-      <div className="flex mt-10 gap-16">
-        <div className="w-3/5">
+      <div className="flex mt-10 md:gap-16 gap-4 md:flex-row flex-col-reverse items-center">
+        <div className="md:w-3/5 w-full">
           <FadeIn>
-            <div className="text-2xl text-white opacity-50 whitespace-pre-line text-justify">
+            <div className="md:text-2xl text-md text-white opacity-50 whitespace-pre-line text-justify">
               Hello, I am Mahartha Gemilang but people usually call me Gilang. I am an undergraduate computer 
               science student at University of Indonesia. As an undergraduate student, I always seek the 
               opportunity to learn new things everyday.
@@ -64,7 +64,7 @@ export default function About() {
           </FadeIn>
             <br/>
           <FadeIn>
-            <div className="text-2xl text-white opacity-50 whitespace-pre-line text-justify">
+            <div className="md:text-2xl text-md text-white opacity-50 whitespace-pre-line text-justify">
               I like to solve new problems and learn new things from 
               it. I do web development eagerly while maintaining my competitive programming skill in my free 
               time. I have participated in numerous competitive programming contests and authored many 
@@ -72,7 +72,7 @@ export default function About() {
             </div>
           </FadeIn>
         </div>
-        <aside className="flex justify-end relative w-2/5 h-fit">
+        <aside className="flex justify-end relative md:w-2/5 w-1/2 h-fit">
           <Image
             ref={el}
             id="profile"
