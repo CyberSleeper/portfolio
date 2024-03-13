a78e4f794ec864f93841b6e6445130a4db068142
CyberSleeper <mahartha.gemilang@gmail.com>
2024-03-13 23:34:23 +0700

    Fix bug the section does not covers properly

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -123,7 +123,7 @@ export default function Skills() {
     <div 
       ref={baseDiv}
       onMouseMove={handleMouseMove}
-      className="bg-light-primary dark:bg-dark-darkGrey px-20 overflow-hidden relative hover:cursor-none"
+      className="z-10 -my-3 bg-light-primary dark:bg-dark-darkGrey px-20 overflow-hidden relative hover:cursor-none"
     >
       <div
         ref={cursorDiv}
