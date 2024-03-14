2927604c532f059b197e460353efe524a81bdde3
CyberSleeper <mahartha.gemilang@gmail.com>
2024-03-14 10:25:15 +0700

    Add responsive to Contact

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -18,7 +18,7 @@ const ContactItem = ({ src, alt, link }: { src: string, alt: string, link: strin
     <Link href={link} className="w-max">
       <div className="flex items-center gap-2" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <Image src={src} draggable='false' alt={alt} width={50} height={50} className={`shadow-2xl rounded-xl border-2 transition-all ${isHovered ? "border-dark-accent" : "border-white"}`}/>
-        <p className={`text-base font-normal transition-all ${isHovered ? "text-dark-accent" : "opacity-95 border-white"}`}>{alt}</p>
+        <p className={`md:flex hidden text-base font-normal transition-all ${isHovered ? "text-dark-accent" : "opacity-95 border-white"}`}>{alt}</p>
       </div>
     </Link>
   )
@@ -26,11 +26,11 @@ const ContactItem = ({ src, alt, link }: { src: string, alt: string, link: strin
 
 const ContactInfo = () => {
   return (
-    <div className="w-96 bg-dark-primary h-[70vh] shadow-2xl rounded-xl -mr-28 z-10 px-12 py-12 text-3xl font-bold text-dark-text">
+    <div className="flex flex-col md:items-start items-center md:w-96 w-full bg-dark-primary h-max shadow-2xl rounded-xl md:-mr-28 z-10 md:px-12 py-12 text-3xl font-bold text-dark-text">
       <h2>
         Contact Me!
       </h2>
-      <div className="mt-12 flex flex-col gap-10">
+      <div className="mt-12 flex md:flex-col flex-row md:gap-10 gap-3">
         <ContactItem src="/contacts/mail_icon.png" alt="mahartha.gemilang@gmail.com" link="mailto:mahartha.gemilang@gmail.com" />
         <ContactItem src="/contacts/instagram_icon.png" alt="@mahartha.gemilang" link="https://www.instagram.com/mahartha.gemilang/" />
         <ContactItem src="/contacts/linkedin_icon.png" alt="mahartha-gemilang" link="https://www.linkedin.com/in/mahartha-gemilang" />
@@ -42,8 +42,8 @@ const ContactInfo = () => {
 
 const ContactForm = () => {
   return (
-    <div className="w-2/3 bg-dark-darkGrey h-[80vh] shadow-2xl rounded-xl pl-36 pr-12 py-12 text-dark-text">
-      <h2 className="text-3xl font-bold">Get in Touch</h2>
+    <div className="w-full bg-dark-darkGrey h-[80vh] shadow-2xl rounded-xl md:pl-36 pl-8 md:pr-12 pr-8 py-12 text-dark-text">
+      <h2 className="md:text-3xl text-3xl font-bold">Get in Touch</h2>
       <form className="mt-12 flex flex-col gap-6">
         <input
           type="text"
@@ -73,7 +73,7 @@ const ContactForm = () => {
 
 export default function Contact() {
   return (
-    <div className="transition-all min-h-screen font-satoshi pb-24 bg-light-background dark:bg-dark-background px-24 pt-16 flex flex-col">
+    <div className="transition-all min-h-screen font-satoshi pb-24 bg-light-background dark:bg-dark-background md:px-24 px-8 pt-16 flex flex-col">
       {/* <FadeIn>
         <h1 className="text-5xl font-bold text-light-secondary dark:text-dark-secondary">Contact Me!</h1>
       </FadeIn> */}
