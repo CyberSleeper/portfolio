ed318c402afd1dc6525aa2a6b33e1b31c4a3ff39
CyberSleeper <mahartha.gemilang@gmail.com>
2024-12-04 02:07:18 +0700

    feat: add experiences

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import FadeIn from "@/components/animations/fadeIn";
-import { 
+import {
   WiMoonAltNew,
-  WiMoonAltWaxingCrescent1, 
-  WiMoonAltWaxingCrescent2, 
-  WiMoonAltWaxingCrescent3, 
-  WiMoonAltWaxingCrescent4, 
-  WiMoonAltWaxingCrescent5, 
+  WiMoonAltWaxingCrescent1,
+  WiMoonAltWaxingCrescent2,
+  WiMoonAltWaxingCrescent3,
+  WiMoonAltWaxingCrescent4,
+  WiMoonAltWaxingCrescent5,
   WiMoonAltWaxingCrescent6,
   WiMoonAltFirstQuarter,
   WiMoonAltWaxingGibbous1,
@@ -16,23 +16,36 @@ import {
   WiMoonAltWaxingGibbous5,
   WiMoonAltWaxingGibbous6,
   WiMoonAltFull,
+  WiMoonAltWaningGibbous1,
+  WiMoonAltWaningGibbous2,
+  WiMoonAltWaningGibbous3,
+  WiMoonAltWaningGibbous4,
+  WiMoonAltWaningGibbous5,
+  WiMoonAltWaningGibbous6,
+  WiMoonAltThirdQuarter,
+  WiMoonAltWaningCrescent1,
+  WiMoonAltWaningCrescent2,
+  WiMoonAltWaningCrescent3,
+  WiMoonAltWaningCrescent4,
+  WiMoonAltWaningCrescent5,
+  WiMoonAltWaningCrescent6,
 } from "react-icons/wi";
 import { useState } from "react";
 import HighlightOnScroll from "@/components/animations/highlightOnScroll";
 import Hyperlink from "@/components/Hyperlink";
 
 interface CompanyProps {
-  name: string
-  website: string
+  name: string;
+  website: string;
 }
 
 interface ExperienceProps {
-  pref: number
-  position: string
-  company: CompanyProps
-  start_date: string
-  end_date: string
-  descriptions: string[]
+  pref: number;
+  position: string;
+  company: CompanyProps;
+  start_date: string;
+  end_date: string;
+  descriptions: string[];
 }
 
 const bulletStyles = [
@@ -51,15 +64,60 @@ const bulletStyles = [
   WiMoonAltWaxingGibbous5,
   WiMoonAltWaxingGibbous6,
   WiMoonAltFull,
-]
+  WiMoonAltWaningGibbous1,
+  WiMoonAltWaningGibbous2,
+  WiMoonAltWaningGibbous3,
+  WiMoonAltWaningGibbous4,
+  WiMoonAltWaningGibbous5,
+  WiMoonAltWaningGibbous6,
+  WiMoonAltThirdQuarter,
+  WiMoonAltWaningCrescent1,
+  WiMoonAltWaningCrescent2,
+  WiMoonAltWaningCrescent3,
+  WiMoonAltWaningCrescent4,
+  WiMoonAltWaningCrescent5,
+  WiMoonAltWaningCrescent6,
+];
 
 const experiences: ExperienceProps[] = [
   {
     pref: 0,
+    position: "Frontend Engineer",
+    company: {
+      name: "Kapaloku",
+      website: "#",
+    },
+    start_date: "Jun 2024",
+    end_date: "Present",
+    descriptions: [
+      "Developed the Admin Panel with 9 primary sections (e.g., Trips, Boats, Sponsorships), each containing multiple subpages for detailed functionality",
+      "Developed the Admin Panel with 3 primary sections (Analytics, Bookings, and Marketings), each containing multiple subpages for detailed functionality",
+      "Built a fully animated Company Profile Page, showcasing the brand's identity and services through engaging visuals and smooth interactions.",
+      "Delivered a scalable, responsive, and user-friendly UI, ensuring high performance and compatibility across devices using Next.js",
+    ],
+  },
+  {
+    pref: 4,
+    position: "Fullstack Engineer",
+    company: {
+      name: "BangSoal",
+      website: "https://bangsoal.co.id/",
+    },
+    start_date: "Jun 2024",
+    end_date: "Sep 2024",
+    descriptions: [
+      "Developed an analytics dashboard for the admin panel using Nest.js and Redis caching to improve query performance.",
+      "Migrated the tryout system backend into the main repository.",
+      "Revamped the tryout system frontend, transforming it to a modern, user-friendly interface based on provided UI/UX designs.",
+      "Led a team to enhance the tryout system, adding support for new question types such as short answers and table-based choices, handling the process from proof of concept to development leadership.",
+    ],
+  },
+  {
+    pref: 8,
     position: "Backend Engineer",
     company: {
       name: "TamanSchool",
-      website: "https://www.tamanschool.com"
+      website: "https://www.tamanschool.com",
     },
     start_date: "Jan 2024",
     end_date: "Apr 2024",
@@ -67,80 +125,79 @@ const experiences: ExperienceProps[] = [
       "Optimized search query for finding classes, reducing search time from over 1 minute to under 3 second.",
       "Maintained the backend services for TamanSchool's platform.",
       "Developed the backend services for Class Making Request using Django.",
-    ]
+    ],
   },
   {
-    pref: 3,
+    pref: 11,
     position: "Fullstack Engineer",
     company: {
       name: "Pemira UI 2024",
-      website: "https://pemiraui.id"
+      website: "https://pemiraui.id",
     },
     start_date: "Oct 2023",
     end_date: "Des 2023",
     descriptions: [
       "Developed the frontend of Pemira UI 2024's website using NextJS and TailwindCSS.",
       "Developed the database schema, seeding, and API for the website using PostgreSQL, Prisma, and ExpressJS.",
-    ]
+    ],
   },
   {
-    pref: 5,
+    pref: 13,
     position: "Lead Software Engineer",
     company: {
       name: "Open House Fasilkom UI 2023",
-      website: "https://oh.cs.ui.ac.id"
+      website: "https://oh.cs.ui.ac.id",
     },
     start_date: "Jul 2023",
     end_date: "Nov 2023",
     descriptions: [
       "Lead a team of 7 developers to develop Open House Fasilkom UI's website.",
       "Developed Registration Page, Wall of Fame Page, and 4 other sections using NextJS and TailwindCSS.",
-      "Deployed the website with reverse proxy and SSL using Nginx and PM2."
-    ]
+      "Deployed the website with reverse proxy and SSL using Nginx and PM2.",
+    ],
   },
   {
-    pref: 8,
+    pref: 16,
     position: "Fullstack Engineer",
     company: {
       name: "PMB Fasilkom UI 2023",
-      website: "https://pmb.cs.ui.ac.id"
+      website: "https://pmb.cs.ui.ac.id",
     },
     start_date: "Jul 2023",
     end_date: "Aug 2023",
     descriptions: [
       "Revamped the user interface of PMB Fasilkom UI's website using TailwindCSS and NextJS.",
-      "Developed the backend service to facilitate seamless friend connection through one-time-use QR code scanning functionality using ExpressJS."
-    ]
+      "Developed the backend service to facilitate seamless friend connection through one-time-use QR code scanning functionality using ExpressJS.",
+    ],
   },
   {
-    pref: 10,
+    pref: 18,
     position: "Software Engineer",
     company: {
       name: "GovTech Procurement | Telkom Indonesia",
-      website: "https://www.eproc-gov.tech"
+      website: "https://www.eproc-gov.tech",
     },
     start_date: "Jun 2023",
     end_date: "Aug 2023",
     descriptions: [
       "Developed reusable real-time database streaming components for analytics purposes using ClickHouse and Metabase.",
-      "Developed session recording on database access for audit purposes using Metabase."
-    ]
+      "Developed session recording on database access for audit purposes using Metabase.",
+    ],
   },
   {
-    pref: 12,
+    pref: 20,
     position: "Fullstack Engineer",
     company: {
       name: "PERAK Fasilkom UI 2023",
-      website: "https://perak.cs.ui.ac.id"
+      website: "https://perak.cs.ui.ac.id",
     },
     start_date: "Mar 2023",
     end_date: "Jun 2023",
     descriptions: [
       "Developed the scoreboard reusable component for PERAK Fasilkom UI's website using TailwindCSS and NextJS.",
-
-    ]
+    ],
   },
-]
+];
 
 interface BulletPointProps {
   index: number;
@@ -152,11 +209,13 @@ const BulletPoint: React.FC<BulletPointProps> = ({ index, children }) => {
   return (
     <div className="flex mb-2 md:gap-1 gap-0.5 md:text-lg text-sm">
       <div className="floatleft md:h-7 h-5 w-auto flex items-center align-middle aspect-square">
-        <BulletStyle size={30} color="#fde68a" className="md:w-6 w-4 h-auto aspect-square" />
+        <BulletStyle
+          size={30}
+          color="#fde68a"
+          className="md:w-6 w-4 h-auto aspect-square"
+        />
       </div>
-      <div className="w-fit float-right">
-        {children}
-      </div>
+      <div className="w-fit float-right">{children}</div>
     </div>
   );
 };
@@ -165,35 +224,41 @@ export default function Experience() {
   return (
     <div className=" transition-all min-h-screen font-satoshi pb-24 bg-dark-background md:px-24 px-12 pt-16 flex flex-col">
       <FadeIn>
-        <h1 className="md:text-5xl text-3xl font-bold text-dark-secondary">My Experience</h1>
+        <h1 className="md:text-5xl text-3xl font-bold text-dark-secondary">
+          My Experience
+        </h1>
       </FadeIn>
       <div className="flex h-max">
-        <div className="w-4 h-max bg-red-500"/>
+        <div className="w-4 h-max bg-red-500" />
         <div>
-          {
-            experiences.map((experience, index) => (
-              <HighlightOnScroll key={index}>
-                <div className="md:mt-14 mt-20">
-                  <h1 className="md:text-3xl text-2xl font-bold text-dark-text">{experience.position}</h1>
-                  <h2 className="md:text-xl text-lg font-bold text-dark-accent">
-                    <Hyperlink
-                      href={experience.company.website}
-                      hexColor="#60C1E3"
-                    >{experience.company.name}</Hyperlink>
-                  </h2>
-                  <h3 className="md:text-xl text-lg font-bold text-dark-text opacity-30">{experience.start_date} - {experience.end_date}</h3>
-                  <div className="mt-4">
-                    {
-                      experience.descriptions.map((description, index) => (
-                        <BulletPoint key={index} index={experience.pref+index}>{description}</BulletPoint>
-                        // <p key={index} className="text-dark-text flex mb-2 gap-3"><WiMoonAltNew size={18} className="mt-1"/> {description}</p>
-                      ))
-                    }
-                  </div>
+          {experiences.map((experience, index) => (
+            <HighlightOnScroll key={index}>
+              <div className="md:mt-14 mt-20">
+                <h1 className="md:text-3xl text-2xl font-bold text-dark-text">
+                  {experience.position}
+                </h1>
+                <h2 className="md:text-xl text-lg font-bold text-dark-accent">
+                  <Hyperlink
+                    href={experience.company.website}
+                    hexColor="#60C1E3"
+                  >
+                    {experience.company.name}
+                  </Hyperlink>
+                </h2>
+                <h3 className="md:text-xl text-lg font-bold text-dark-text opacity-30">
+                  {experience.start_date} - {experience.end_date}
+                </h3>
+                <div className="mt-4">
+                  {experience.descriptions.map((description, index) => (
+                    <BulletPoint key={index} index={experience.pref + index}>
+                      {description}
+                    </BulletPoint>
+                    // <p key={index} className="text-dark-text flex mb-2 gap-3"><WiMoonAltNew size={18} className="mt-1"/> {description}</p>
+                  ))}
                 </div>
-              </HighlightOnScroll>
-            ))
-          }
+              </div>
+            </HighlightOnScroll>
+          ))}
         </div>
       </div>
     </div>
